144ed609beabd6ce0046364af5df9df03a135409
Damian <damiannelus@interia.pl>
2020-07-19 08:10:02 +0200

    Added Faker library

---
+++ tests/unit/flashCardTests.js
@@ -0,0 +1,9 @@
+import chai, { expect } from 'chai';
+import flashCard from '../../src/data_objects/flashCard';
+import faker from 'faker';
+
+// chai.should()
+
+describe("test flashcards", () => {
+  
+})
